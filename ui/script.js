5ab8addf2d5cea97db3da3008575e453b6c98860
Leticia <leticia.nkulu@gmail.com>
2019-05-13 05:45:09 +0300

    feat: add the signup modal for riders
    This will allow users to signup as riders after clicking the "A rider"
    button on the homepage
    Closes: #2

---
+++ ui/script.js
@@ -0,0 +1,9 @@
+
+
+  function openForm() {
+    document.getElementById("rider-signup-form").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("rider-signup-form").style.display = "none";
+  }
