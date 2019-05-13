163e6e27a8882ad4d22b55d1b4738386d54900bd
Leticia <leticia.nkulu@gmail.com>
2019-05-13 06:03:56 +0300

    feat: add a driver signup modal to the homepage
    This added modal will allow users to signup as drivers
    Closes: #3

--- ui/script.js
+++ ui/script.js
@@ -1,9 +1,18 @@
 
 
-  function openForm() {
+  function openriderForm() {
     document.getElementById("rider-signup-form").style.display = "block";
   }
   
-  function closeForm() {
+  function closeriderForm() {
     document.getElementById("rider-signup-form").style.display = "none";
+  }
+
+
+  function opendriverForm() {
+    document.getElementById("driver-signup-form").style.display = "block";
+  }
+  
+  function closedriverForm() {
+    document.getElementById("driver-signup-form").style.display = "none";
   }
